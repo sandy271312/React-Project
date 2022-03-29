afaaaffe592cc91eda3ed99802c256d15cbec106
khyamling <pkhyamling@gmail.com>
2022-03-29 11:48:55 +0530

    `

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Make React Project
         </a>
       </header>
     </div>
