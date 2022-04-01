29fdba7efa94b7d356d59f81aa16c226cfe19d18
Sandeep Mishra <gm271312@gmial.com>
2022-04-01 11:31:25 +0530

    TextForm is added with Uppercase function

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -24,7 +24,6 @@ export default function TextForms(props) {
         <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
         </div>
         <button className='btn btn-primary' onClick={handleUpClick} >Convert to uppercase</button>
-      
     </div>
   )
 }
