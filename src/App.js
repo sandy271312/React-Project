4d88dbde76d268f78ca447cd4e846fa4294efe15
Sandeep Mishra <gm271312@gmial.com>
2022-04-05 15:51:09 +0530

    Component About is added to enable the dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './components/ Navbar';
-import TextForms from './components/TextForms'
+import About from './components/About';
+//import TextForms from './components/TextForms'
 export function App() {
   return (
     <>
       {/* <Navbar /> */}
       <Navbar title="textUtils" about="about textUtils" />
       <div className='container my-3'>
-        <TextForms heading="Enter the text to analyze below"/>
+        {/* <TextForms heading="Enter the text to analyze below"/> */}
+        <About/>
       </div>
           
     </>
