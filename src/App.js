155c048494edc0826b536a46b9effbd16f7cca76
Sandeep Mishra <gm271312@gmial.com>
2022-04-05 15:53:41 +0530

    About component is not used currently

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from './components/ Navbar';
 import About from './components/About';
-//import TextForms from './components/TextForms'
+import TextForms from './components/TextForms'
 export function App() {
   return (
     <>
       {/* <Navbar /> */}
       <Navbar title="textUtils" about="about textUtils" />
       <div className='container my-3'>
-        {/* <TextForms heading="Enter the text to analyze below"/> */}
-        <About/>
+        <TextForms heading="Enter the text to analyze below"/>
+        {/* <About/> */}
       </div>
           
     </>
