58f290994ea908be2e57d9a49c51e8c95a50539e
Sandeep Mishra <gm271312@gmial.com>
2022-04-06 12:02:00 +0530

    Navebar.js: Dark mode enable switch is added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Navbar from './components/ Navbar';
 //import About from './components/About';
+
+
 import TextForms from './components/TextForms'
 export function App() {
+  const [mode,setMode]=useState('light');
+
+const togglemode=()=>{
+       if(mode === 'light') {
+          setMode('dark');
+         document.body.style.backgroundColor='#042743';}
+
+      else{ 
+         setMode('light')
+           document.body.style.backgroundColor='white';
+      }
+    }
+      
+
   return (
     <>
       {/* <Navbar /> */}
-      <Navbar title="textUtils" about="about textUtils" />
+      <Navbar title="textUtils" mode={mode} togglemode={togglemode} about="about textUtils" />
       <div className='container my-3'>
-        <TextForms heading="Enter the text to analyze below"/>
+        <TextForms mode={mode} heading="Enter the text to analyze below"/>
         {/* <About/> */}
       </div>
           
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -45,10 +45,10 @@ export default function TextForms(props) {
   const [text, setText] = useState('');
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color:props.mode==='dark'?'white':'#042743'}}>
         <h2>{props.heading}</h2>
         <div className="mb-3">
-        <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+        <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743'}}></textarea>
         </div>
         <button className='btn btn-primary mx-1' onClick={handleUpClick} >Convert to uppercase</button>
         <button className='btn btn-primary mx-1' onClick={handleLowerClick} >Convert to lowercase</button>
@@ -65,12 +65,12 @@ export default function TextForms(props) {
        
 
     </div>
-    <div className='container my-3'>
+    <div className='container my-3'style={{color:props.mode==='dark'?'white':'#42743'}}>
       <h2>Your Text Summery</h2>
       <p>{text.split(" ").length} words and {text.length} character</p>
       <p>{0.008*text.split(" ").length} minutes read</p>
-      <p>Preview</p>
-      <>{text}</>
+      <h2>Preview</h2>
+      <>{text.length>0 ? text:"Enter something into the text box above to perview it here"}</>
 
     </div>
     </>
